7dcd81f0df3666fbd703bbcfa25c2f255aad359a
ogieproject-sketch <ogieproject@gmail.com>
2026-07-26 23:35:29 +0800

    Add new brand favicon icon

--- wp-content/themes/lefanna/functions.php
+++ wp-content/themes/lefanna/functions.php
@@ -12,6 +12,13 @@ add_action('after_setup_theme', function() {
     add_theme_support('block-templates');
 });
 
+// Add Favicon to wp_head
+add_action('wp_head', function() {
+    echo '<link rel="icon" type="image/png" href="/wp-content/themes/lefanna/images/favicon.png" />' . "\n";
+    echo '<link rel="shortcut icon" type="image/png" href="/wp-content/themes/lefanna/images/favicon.png" />' . "\n";
+    echo '<link rel="apple-touch-icon" href="/wp-content/themes/lefanna/images/favicon.png" />' . "\n";
+});
+
 // Clear template customizations from the database to force WordPress to load the HTML files from disk
 add_action('init', function() {
     if (isset($_GET['clear_templates'])) {
