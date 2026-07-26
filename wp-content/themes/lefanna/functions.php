190fed2d551b0f29c77f44c43177432887ccc765
ogieproject-sketch <ogieproject@gmail.com>
2026-07-26 09:18:49 +0800

    Update tema Lefanna dan template halaman baru

---
+++ wp-content/themes/lefanna/functions.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Lefanna Theme Functions
+ */
+
+add_action('after_setup_theme', function() {
+    add_theme_support('wp-block-styles');
+    add_theme_support('editor-styles');
+    add_theme_support('responsive-embeds');
+    add_theme_support('html5', array('comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'style', 'script'));
+    add_theme_support('post-thumbnails');
+    add_theme_support('block-templates');
+});
+
+// Clear template customizations from the database to force WordPress to load the HTML files from disk
+add_action('init', function() {
+    if (isset($_GET['clear_templates'])) {
+        $posts = get_posts(array(
+            'post_type' => array('wp_template', 'wp_template_part'),
+            'post_status' => 'any',
+            'numberposts' => -1
+        ));
+        foreach ($posts as $post) {
+            wp_delete_post($post->ID, true);
+        }
+        echo "Successfully cleared database templates! Please refresh the page.";
+        exit;
+    }
+});
--- wp-content/themes/lefanna/index.php
+++ wp-content/themes/lefanna/index.php
@@ -1,2 +1,18 @@
 <?php
-// Silence is golden. Block themes use HTML templates in the templates directory.
+/**
+ * Main Template File
+ * Lefanna Theme
+ */
+
+$front_page = __DIR__ . '/templates/front-page.html';
+$index_page = __DIR__ . '/templates/index.html';
+
+if ( file_exists( $front_page ) ) {
+    include $front_page;
+} elseif ( file_exists( $index_page ) ) {
+    include $index_page;
+} else {
+    wp_head();
+    echo "<h1>Lefanna Theme</h1>";
+    wp_footer();
+}
